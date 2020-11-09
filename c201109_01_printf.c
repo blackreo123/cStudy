c913614b76cdc11091def279ce5510e2acca0479
jiha <blackreo123@gmail.com>
2020-11-09 15:19:56 +0900

    변수 공부 추가

--- c201109_01_printf.c
+++ c201109_01_printf.c
@@ -1,7 +1,16 @@
 #include <stdio.h> // stdio.h: 표준입출력 헤더
 
+//진입점 함수 반드시 프로젝트에서 한개만 존재
 int main(){
     printf("c를 배워봅시다!!!!!!!!!!!!!!\n");
     printf("다음줄에 출력\n");
+    printf("%d + %d = %d\n",10,20,10+20);
+    printf("원주율은 %.2f\n",3.14);
+    printf("1000%c\n",'$');
+    printf("나의 이름은 %s\n","윤지하");
+    printf("저는 대한민국 사람입니다.\n");
+    printf("my age is %d\n",29);
+    
+    
     return 0;
 }
