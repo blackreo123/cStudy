c62a50269dbe7a069928c4599d0912afac4aa4c8
jiha <blackreo123@gmail.com>
2020-11-11 13:13:59 +0900

    while

---
+++ c201111_02_while.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+    //while
+    // int a = 0;
+    // while (a < 10){
+    //     printf("%d\n",++a);
+    // }
+
+    //1~10 짝수만 출력
+    // int a = 0;
+    // while (a < 10)
+    // {
+    //     a++;
+    //     if (a % 2 == 0)
+    //     {
+    //         printf("%d\n", a);
+    //     }
+    // }
+
+    //continue 사용하여 1~10까지 짝수만
+    // int a = 0;
+    // while (a < 10)
+    // {
+    //     a++;
+    //     if (a % 2 != 0)
+    //     {
+    //         continue;
+    //     }
+    //     else
+    //     {
+    //         printf("%d\n", a);
+    //     }
+    // }
+
+    //합계구하기
+    // int a = 0;
+    // int sum = 0;
+    // while(a < 10){
+    //     sum += ++a;
+    // }
+    // printf("합계 : %d",sum);
+    // return 0;
+
+    // 1~20까지 3의 배수만
+    // int a = 0;
+    // while (a < 20)
+    // {
+    //     a++;
+    //     if(a % 3 == 0){
+    //         printf("%d\n",a);
+    //     }else{
+    //         continue;
+    //     }
+    // }
+    
+
+}
