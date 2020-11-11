8f3015383b1004012086ddcdf35493b89b166210
jiha <blackreo123@gmail.com>
2020-11-11 12:00:38 +0900

    switch

--- c201111_01_switch.c
+++ c201111_01_switch.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    //switch
+    // switch
     // int no = 2;
     // switch (no)
     // {
@@ -24,31 +24,60 @@ int main()
     // default:
     //     printf("잘못 입력하셨습니다.\n");
     //     break;
-    // }
+    //}
 
     //마지막날을 출력
-    //
-
-    //A~F
-    char g;
-    printf("your grade is : ");
-    scanf("%c", &g);
-    switch (g)
-    {
-    case 'A':
-    case 'B':
-        printf("GREAT!");
-        break;
-    case 'C':
-    case 'D':
-        printf("GOOD!");
-        break;
-    case 'E':
-    case 'F':
-        printf("TRY!");
-        break;
-    default:
-        printf("check again!");
+    int year, mon;
+    printf("which year : ");
+    scanf("%d",&year);
+    printf("which month : ");
+    scanf("%d",&mon);
+    switch(mon){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31");
+            break;
+        case 2:
+            if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+                printf("29");
+            }else{
+                printf("28");
+            }
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("30");
+            break;
+        default:
+            printf("wrong");
     }
+    //A~F
+    // char g;
+    // printf("your grade is : ");
+    // scanf("%c", &g);
+    // switch (g)
+    // {
+    // case 'A':
+    // case 'B':
+    //     printf("GREAT!");
+    //     break;
+    // case 'C':
+    // case 'D':
+    //     printf("GOOD!");
+    //     break;
+    // case 'E':
+    // case 'F':
+    //     printf("TRY!");
+    //     break;
+    // default:
+    //     printf("check again!");
+    //}
     return 0;
 }
